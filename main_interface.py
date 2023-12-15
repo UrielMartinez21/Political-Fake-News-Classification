adfeaf24afa5e65de44a2987a11f0f7f289ae8ab
UrielMartinez21 <uriellmartinezz35@gmail.com>
2023-12-15 17:12:08 -0600

    feat(main_interface.py):creacion de documento pdf

--- main_interface.py
+++ main_interface.py
@@ -1,10 +1,15 @@
 # -----------------------| Bibliotecas |-----------------------
-import tkinter
-from PIL import ImageTk, Image
 import torch
-from transformers import RobertaTokenizer, RobertaForSequenceClassification
 import random
+import tkinter
+from datetime import datetime
 import numpy as np
+from PIL import ImageTk, Image
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.pagesizes import letter
+from transformers import RobertaTokenizer, RobertaForSequenceClassification
 
 # -----------------------| Ejecutar al inicio |-----------------------
 # Configurar semilla
@@ -30,10 +35,22 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = model.to(device)
 
 # -----------------------| Funciones |-----------------------
+def validar_campos_llenos():
+    """
+    Verifica si los campos obligatorios están llenos
+    """
+    return bool(input_titulo.get() and input_descripcion.get() and input_fecha.get())
+
+
 def detectar_noticia():
     """
     Identificar si la noticia es falsa o verdadera
     """
+    # Validar si los campos están llenos
+    if not validar_campos_llenos():
+        resultado_label.config(text="Por favor, completa todos los campos.", bg="red", fg="white")
+        return
+
     # --> Obtener datos
     titulo = input_titulo.get()
     descripcion = input_descripcion.get()
@@ -71,6 +88,74 @@ def detectar_noticia():
         resultado_label.config(text="La noticia es FALSA", bg="red", fg="white")
     else:
         resultado_label.config(text="La noticia es VERDADERA", bg="green", fg="white")
+    
+    # Genera un PDF con la información
+    generar_pdf(titulo, descripcion, fecha, etiqueta_predicha)
+
+
+def generar_pdf(titulo, descripcion, fecha, etiqueta):
+    """
+    Genera un PDF con la información de la noticia
+    """
+    # Crear el nombre del archivo PDF con la fecha actual
+    fecha_actual = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+    pdf_filename = f"noticia_{fecha_actual}.pdf"
+
+
+    # Crear el documento PDF
+    doc = SimpleDocTemplate(pdf_filename, pagesize=letter)
+    width, height = letter
+
+    # Lista para almacenar los elementos del PDF
+    story = []
+
+    # Establecer estilos de texto
+    styles = getSampleStyleSheet()
+
+    # Encabezado
+    encabezado_texto = "<b>Detector de noticias falsas</b>"
+    story.append(Paragraph(encabezado_texto, styles["Normal"]))
+
+    # Linea de separación
+    story.append(Paragraph("<br/><br/>"))
+
+    # Título
+    titulo_texto = "<b>Título:</b><br/>" + titulo
+    story.append(Paragraph(titulo_texto, styles["Normal"]))
+
+    # Linea de separación
+    story.append(Paragraph("<br/><br/>"))
+
+    # Descripción
+    descripcion_texto = "<b>Descripción:</b><br/>" + descripcion
+    story.append(Paragraph(descripcion_texto, styles["Normal"]))
+
+    # Linea de separación
+    story.append(Paragraph("<br/><br/>"))
+
+    # Fecha
+    fecha_texto = "<b>Fecha:</b><br/>" + fecha
+    story.append(Paragraph(fecha_texto, styles["Normal"]))
+
+    # Linea de separación
+    story.append(Paragraph("<br/><br/>"))
+
+    # Etiqueta
+    etiqueta_texto = "<b>La noticia es:</b>" + etiqueta
+    story.append(Paragraph(etiqueta_texto, styles["Normal"]))
+
+    # Construir el PDF
+    doc.build(story)
+
+    print(f"Se ha generado el PDF: {pdf_filename}")
+
+
+# Boton para limpiar los inputs
+def limpiar_inputs():
+    input_titulo.delete(0, 'end')
+    input_descripcion.delete(0, 'end')
+    input_fecha.delete(0, 'end')
+    resultado_label.config(text="", bg="white", fg="black")
 
 
 # -----------------------| Inicio de interfaz |-----------------------
@@ -119,7 +204,11 @@ resultado_label.place(relx=0.25, rely=0.8, relwidth=0.5, relheight=0.05)
 
 # Boton para ejecutar el programa
 boton_detectar = tkinter.Button(text="Detectar", bg="white", font="Arial 12 bold", fg="black", command=detectar_noticia)
-boton_detectar.place(relx=0.45, rely=0.7, relwidth=0.2, relheight=0.05)
+boton_detectar.place(relx=0.60, rely=0.7, relwidth=0.2, relheight=0.05)
+
+# Boton para limpiar los inputs
+boton_limpiar = tkinter.Button(text="Limpiar", bg="white", font="Arial 12 bold", fg="black", command=limpiar_inputs)
+boton_limpiar.place(relx=0.30, rely=0.7, relwidth=0.2, relheight=0.05)
 
 # --> Ejecutar ventana
 ventana.mainloop()
