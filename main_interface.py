9e036b760becfb191df5b88517cd25d0ded3126a
UrielMartinez21 <uriellmartinezz35@gmail.com>
2023-12-16 10:25:49 -0600

    feat(main_interface.py):ajuste de colores

--- main_interface.py
+++ main_interface.py
@@ -1,15 +1,17 @@
 # -----------------------| Bibliotecas |-----------------------
+# --> Bibliotecas para interfaz
+import tkinter
+from PIL import ImageTk, Image
+# --> Bibliotecas para el modelo
 import torch
 import random
-import tkinter
-from datetime import datetime
 import numpy as np
-from PIL import ImageTk, Image
+from transformers import RobertaTokenizer, RobertaForSequenceClassification
+# --> Bibliotecas para PDF
+from datetime import datetime
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.lib.pagesizes import letter
-from transformers import RobertaTokenizer, RobertaForSequenceClassification
 
 # -----------------------| Ejecutar al inicio |-----------------------
 # Configurar semilla
@@ -155,7 +157,7 @@ def limpiar_inputs():
     input_titulo.delete(0, 'end')
     input_descripcion.delete(0, 'end')
     input_fecha.delete(0, 'end')
-    resultado_label.config(text="", bg="white", fg="black")
+    resultado_label.config(text="", bg="#231B12", fg="black")
 
 
 # -----------------------| Inicio de interfaz |-----------------------
@@ -165,49 +167,49 @@ ventana = tkinter.Tk()
 ventana.geometry("700x700")
 ventana.title("Detector de noticias falsas")
 ventana.iconbitmap("diseño/logo/logo.ico")
-ventana.config(bg="white")
+ventana.config(bg="#231B12")
 
 # --> Contenido de ventana
 
 # Icono en pantalla
-imagen_logo = ImageTk.PhotoImage(Image.open("diseño/logo/logo.ico"))        # Se abre la imagen
-label_imagen = tkinter.Label(image=imagen_logo)                             # Se crea un label para la imagen
-label_imagen.place(relx=0.20, rely=0.05, relwidth=0.60, relheight=0.3)     # Se coloca el label en la ventana
+imagen_logo = ImageTk.PhotoImage(Image.open("diseño/logo/logo.ico"))
+label_imagen = tkinter.Label(image=imagen_logo, bg="#231B12")  # Mismo color que el fondo principal
+label_imagen.place(relx=0.20, rely=0.05, relwidth=0.60, relheight=0.3)
 
-# Label de titulo
-label_titulo = tkinter.Label(text="Título: ", bg="white", font="Arial 12 bold", fg="black")
+# Label de título
+label_titulo = tkinter.Label(text="Título: ", bg="#B8D20B", font="Caviar 12 bold", fg="#231B12")  # Texto oscuro sobre fondo claro
 label_titulo.place(relx=0.2, rely=0.4, relwidth=0.15, relheight=0.05)
 
-# Input para titulo
-input_titulo = tkinter.Entry()
+# Input para título
+input_titulo = tkinter.Entry(bg="#B8D20B", font="Caviar")  # Fondo claro
 input_titulo.place(relx=0.4, rely=0.4, relwidth=0.4, relheight=0.05)
 
-# Label para descrpcion
-label_descripcion = tkinter.Label(text="Descripción: ", bg="white", font="Arial 12 bold", fg="black")
-label_descripcion.place(relx=0.2, rely=0.5, relwidth=0.15, relheight=0.05)    
+# Label para descripción
+label_descripcion = tkinter.Label(text="Descripción: ", bg="#B8D20B", font="Caviar 12 bold", fg="#231B12")
+label_descripcion.place(relx=0.2, rely=0.5, relwidth=0.15, relheight=0.05)
 
-# Input para descripcion
-input_descripcion = tkinter.Entry()
+# Input para descripción
+input_descripcion = tkinter.Entry(bg="#B8D20B", font="Caviar")  # Fondo claro
 input_descripcion.place(relx=0.4, rely=0.5, relwidth=0.4, relheight=0.05)
 
 # Label para fecha
-label_fecha = tkinter.Label(text="Fecha: ", bg="white", font="Arial 12 bold", fg="black")
+label_fecha = tkinter.Label(text="Fecha: ", bg="#B8D20B", font="Caviar 12 bold", fg="#231B12")
 label_fecha.place(relx=0.2, rely=0.6, relwidth=0.15, relheight=0.05)
 
 # Input para fecha
-input_fecha = tkinter.Entry()
+input_fecha = tkinter.Entry(bg="#B8D20B", font="Caviar")  # Fondo claro
 input_fecha.place(relx=0.4, rely=0.6, relwidth=0.4, relheight=0.05)
 
 # Label para mostrar resultados
-resultado_label = tkinter.Label(ventana, text="", bg="white", font="Arial 12 bold", fg="black")
+resultado_label = tkinter.Label(ventana, text="", bg="#231B12", font="Caviar 12 bold", fg="#FFFFFF")  # Texto blanco sobre fondo oscuro
 resultado_label.place(relx=0.25, rely=0.8, relwidth=0.5, relheight=0.05)
 
-# Boton para ejecutar el programa
-boton_detectar = tkinter.Button(text="Detectar", bg="white", font="Arial 12 bold", fg="black", command=detectar_noticia)
+# Botón para ejecutar el programa
+boton_detectar = tkinter.Button(text="Detectar", bg="#F25CA2", font="Caviar 12 bold", fg="#231B12", command=detectar_noticia)
 boton_detectar.place(relx=0.60, rely=0.7, relwidth=0.2, relheight=0.05)
 
-# Boton para limpiar los inputs
-boton_limpiar = tkinter.Button(text="Limpiar", bg="white", font="Arial 12 bold", fg="black", command=limpiar_inputs)
+# Botón para limpiar los inputs
+boton_limpiar = tkinter.Button(text="Limpiar", bg="#69D2E7", font="Caviar 12 bold", fg="#231B12", command=limpiar_inputs)
 boton_limpiar.place(relx=0.30, rely=0.7, relwidth=0.2, relheight=0.05)
 
 # --> Ejecutar ventana
